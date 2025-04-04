1107094f0fbfec8eaf8fa4d1b6d2a7fa76901785
unknown <{ID}+{username}@users.noreply.github.com>
2025-04-03 22:17:35 -0300

    pomodoro tasks initial

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,5 +1,49 @@
+import "./style.scss";
+
+interface ITask {
+  id: number;
+  description: string;
+  completed: string;
+}
+
+interface ApplicationState {
+  tasks: ITask[];
+  taskSelected: ITask | null;
+  editing: boolean;
+}
+
+let initialState = {
+  tasks: [
+    {
+      id: 0,
+      description: "Estudar Angular",
+      completed: false,
+    },
+    {
+      id: 1,
+      description: "Estudar React",
+      completed: true,
+    },
+    {
+      id: 2,
+      description: "Arrumar o quarto",
+      completed: false,
+    },
+  ],
+};
+
 const Tasks = () => {
-  return <></>;
+  return (
+    <section className="app__section-tasks-container">
+      <span className="app__task-selected">#Em andamento</span>
+
+      <ul className="app__tasks-list">
+        {initialState.tasks.map((task) => (
+          <li>{task.description}</li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Tasks;
